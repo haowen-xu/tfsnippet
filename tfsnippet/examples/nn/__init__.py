58ba84741428c2c0b2a4622a16272221874ff909
Haowen Xu <haowen.xu@outlook.com>
2018-07-22 20:21:15 +0800

    added "planar_normalizing_flow", but still need to figure out how to integrate it into the BayesianNet interface

--- tfsnippet/examples/nn/__init__.py
+++ tfsnippet/examples/nn/__init__.py
@@ -1,13 +1,14 @@
-from . import convolutional, losses, regularizers, wrapper
+from . import convolutional, losses, normalization_flows, regularizers, wrapper
 
 __all__ = sum(
     [m.__all__ for m in [
-         convolutional, losses, regularizers, wrapper
+         convolutional, losses, normalization_flows, regularizers, wrapper
     ]],
     []
 )
 
 from .convolutional import *
 from .losses import *
+from .normalization_flows import *
 from .regularizers import *
 from .wrapper import *
