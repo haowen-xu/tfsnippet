532f8316d7c23dc3b9c57d51e14f7e113598ce4d
Haowen Xu <haowen.xu@outlook.com>
2018-12-18 00:25:17 +0800

    fix travis ci script

--- scripts/travis-run-tests.sh
+++ scripts/travis-run-tests.sh
@@ -1,23 +1,42 @@
 #!/bin/bash
 
+function runTest() {
+  PY_VER="$1"
+  TF_VER="$2"
+  RUN_EXAMPLES_TEST_CASE="$3"
 
-if [[ "${TRAVIS_BRANCH}" = "master" || "${TRAVIS_BRANCH}" = "develop" ]]; then
-  if [[ "${CORE_TESTS_ONLY}" = "1" ]]; then
-    export RUN_EXAMPLES_TEST_CASE=0;
+  echo "TFSnippet Tests
+
+    CORE_TESTS_ONLY=${CORE_TESTS_ONLY}
+    PYTHON_VERSION=${PY_VER}
+    TENSORFLOW_VERSION=${TF_VER}
+  "
+
+  IMAGE_NAME="haowenxu/travis-tensorflow-docker:py${PY_VER}tf${TF_VER}"
+  docker pull "${IMAGE_NAME}"
+  docker run
+      -v "$(pwd)":"$(pwd)"
+      -v "/home/travis/.tfsnippet":"/root/.tfsnippet"
+      -v "/home/travis/.keras":"/root/.keras"
+      -w "$(pwd)"
+      -e TRAVIS="${TRAVIS}"
+      -e TRAVIS_JOB_ID="${TRAVIS_JOB_ID}"
+      -e TRAVIS_BRANCH="${TRAVIS_BRANCH}"
+      -e CORE_TESTS_ONLY="${CORE_TESTS_ONLY}"
+      -e RUN_EXAMPLES_TEST_CASE="${RUN_EXAMPLES_TEST_CASE}"
+      "${IMAGE_NAME}"
+      bash "scripts/travis-docker-entry.sh"
+}
+if [[ "${CORE_TESTS_ONLY}" = "1" ]]; then
+  for PY_VER in 2.7 3.5; do
+    for TF_VER in 1.5 1.6 1.7 1.8 1.9 1.10 1.11 1.12; do
+      runTest "${PY_VER}" "${TF_VER}" "0";
+    done
+  done
+else
+  if [[ "${TRAVIS_BRANCH}" = "master" || "${TRAVIS_BRANCH}" = "develop" ]]; then
+    runTest "${PYTHON_VERSION}" "${TENSORFLOW_VERSION}" "1";
   else
-    export RUN_EXAMPLES_TEST_CASE=1;
+    runTest "${PYTHON_VERSION}" "${TENSORFLOW_VERSION}" "0";
   fi
 fi
-
-docker run
-    -v "$(pwd)":"$(pwd)"
-    -v "/home/travis/.tfsnippet":"/root/.tfsnippet"
-    -v "/home/travis/.keras":"/root/.keras"
-    -w "$(pwd)"
-    -e TRAVIS="${TRAVIS}"
-    -e TRAVIS_JOB_ID="${TRAVIS_JOB_ID}"
-    -e TRAVIS_BRANCH="${TRAVIS_BRANCH}"
-    -e CORE_TESTS_ONLY="${CORE_TESTS_ONLY}"
-    -e RUN_EXAMPLES_TEST_CASE="${RUN_EXAMPLES_TEST_CASE}"
-    "haowenxu/travis-tensorflow-docker:py${PYTHON_VERSION}tf${TENSORFLOW_VERSION}"
-    bash "scripts/travis-docker-entry.sh"
