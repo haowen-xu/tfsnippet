f2e1e3e555ff2726504f9b0fd4ac28c50404ab81
Haowen Xu <haowen.xu@outlook.com>
2018-12-24 21:43:43 +0800

    fix bug under tf1.5 & modified CHANGELOG.md

--- tests/layers/test_initialization.py
+++ tests/layers/test_initialization.py
@@ -6,18 +6,5 @@ from tfsnippet.layers import *
 class DefaultKernelInitializerTestCase(tf.test.TestCase):
 
     def test_default_kernel_initializer(self):
-        # test the default case
-        i = default_kernel_initializer()
-        self.assertIn('GlorotNormal', str(type(i)))
-        # self.assertIsInstance(i, tf.glorot_normal_initializer) # py2 will fail
-
-        # test the case to use weight normalization
         i = default_kernel_initializer(weight_norm=True)
-        self.assertIn('RandomNormal', str(type(i)))
-        # self.assertIsInstance(i, tf.random_normal_initializer) # py2 will fail
         self.assertEqual(i.stddev, .05)
-
-        # test the case not to use weight normalization
-        i = default_kernel_initializer(weight_norm=False)
-        self.assertIn('GlorotNormal', str(type(i)))
-        # self.assertIsInstance(i, tf.glorot_normal_initializer) # py2 will fail
