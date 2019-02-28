fae0fa06bec6920ede2edfadbd6d16bfeb1a89b8
Haowen Xu <haowen.xu@outlook.com>
2019-03-01 01:42:18 +0800

    add some test

--- tests/layers/convolutional/test_pixelcnn.py
+++ tests/layers/convolutional/test_pixelcnn.py
@@ -73,7 +73,7 @@ class PixelCNN2DTestCase(tf.test.TestCase):
                                             '`PixelCNN2DOutput`: got 123'):
             _ = pixelcnn_2d_output(123)
 
-    def test_pixelcnn_conv2d_resnet_deps(self):
+    def test_pixelcnn_conv2d_resnet_one_layer_dep(self):
         tf.set_random_seed(1234)
 
         H, W = 11, 12
@@ -85,8 +85,8 @@ class PixelCNN2DTestCase(tf.test.TestCase):
                     idx = i * W + j
 
                     # vertical stack influences
-                    for h in range(H):
-                        for w in range(W):
+                    for h in range(vertical.shape[1]):
+                        for w in range(vertical.shape[2]):
                             has_value = (vertical[idx] != 0.)[h, w]
                             expect_value = ((h - 3 <= i <= h - 1) and
                                             (w - 2 <= j <= w + 2))
@@ -98,8 +98,8 @@ class PixelCNN2DTestCase(tf.test.TestCase):
                             )
 
                     # horizontal stack influences
-                    for h in range(H):
-                        for w in range(W):
+                    for h in range(horizontal.shape[1]):
+                        for w in range(horizontal.shape[2]):
                             has_value = (horizontal[idx] != 0.)[h, w]
                             expect_value = (
                                 # value from vertical stack
@@ -155,3 +155,83 @@ class PixelCNN2DTestCase(tf.test.TestCase):
         with pytest.raises(TypeError, match='`input` is not an instance of '
                                             '`PixelCNN2DOutput`: got 123'):
             _ = pixelcnn_conv2d_resnet(123, out_channels=1)
+
+    def test_pixelcnn_conv2d_resnet_multi_layer_dep(self):
+        tf.set_random_seed(1234)
+
+        H, W = 6, 7
+        x = np.eye(H * W, dtype=np.float32).reshape([H * W, H, W, 1])
+
+        def check_influence(vertical, horizontal):
+            for i in range(H):
+                for j in range(W):
+                    idx = i * W + j
+
+                    # vertical stack influences
+                    for h in range(vertical.shape[1]):
+                        for w in range(vertical.shape[2]):
+                            has_value = (vertical[idx] != 0.)[h, w]
+                            expect_value = i <= h - 1
+                            self.assertEqual(
+                                has_value,
+                                expect_value,
+                                msg='Vertical stack not match at '
+                                    '({},{},{},{})'.format(i, j, h, w)
+                            )
+
+                    # horizontal stack influences
+                    for h in range(horizontal.shape[1]):
+                        for w in range(horizontal.shape[2]):
+                            has_value = (horizontal[idx] != 0.)[h, w]
+                            expect_value = (
+                                # value from vertical stack
+                                i <= h - 1 or
+                                # value from horizontal stack
+                                (j <= w - 1 and i <= h)
+                            )
+                            self.assertEqual(
+                                has_value,
+                                expect_value,
+                                msg='Horizontal stack not match at '
+                                    '({},{},{},{})'.format(i, j, h, w)
+                            )
+
+        with mock.patch('tensorflow.nn.conv2d', patched_conv2d), \
+                self.test_session() as sess:
+            n_layers = 3
+
+            # NHWC
+            y = o = pixelcnn_2d_input(
+                x, channels_last=True, auxiliary_channel=False)
+            for i in range(n_layers):
+                o = pixelcnn_conv2d_resnet(
+                    o, out_channels=1, vertical_kernel_size=(2, 3),
+                    horizontal_kernel_size=(2, 2), strides=(1, 1),
+                    channels_last=True,
+                )
+            ensure_variables_initialized()
+            vertical, horizontal = sess.run([o.vertical, o.horizontal])
+            check_influence(
+                vertical.reshape(vertical.shape[:-1]),
+                horizontal.reshape(horizontal.shape[:-1])
+            )
+
+            # NCHW
+            y = o = pixelcnn_2d_input(
+                np.transpose(x, [0, 3, 1, 2]), channels_last=False,
+                auxiliary_channel=False
+            )
+            for i in range(n_layers):
+                o = pixelcnn_conv2d_resnet(
+                    o, out_channels=1, vertical_kernel_size=(2, 3),
+                    horizontal_kernel_size=(2, 2), strides=(1, 1),
+                    channels_last=False, activation_fn=tf.nn.leaky_relu
+                )
+            ensure_variables_initialized()
+            vertical, horizontal = sess.run([o.vertical, o.horizontal])
+            vertical = np.transpose(vertical, [0, 2, 3, 1])
+            horizontal = np.transpose(horizontal, [0, 2, 3, 1])
+            check_influence(
+                vertical.reshape(vertical.shape[:-1]),
+                horizontal.reshape(horizontal.shape[:-1])
+            )
